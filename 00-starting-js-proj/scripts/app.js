6d207dccf6b839f0a8b354fd6f98ac289c7ded4a
Anish Shukla <shuklaanish040@gmail.com>
2025-01-21 20:53:43 +0530

    udemy

---
+++ 00-starting-js-proj/scripts/app.js
@@ -0,0 +1,8 @@
+// import { apiKey } from "./util.js";
+import { apiKey, abc as content } from "./util.js";
+console.log(apiKey, content);
+import * as util from "./util.js";
+console.log(util.xyz);
+
+import { xyz } from "./util.js";
+console.log(xyz);
---
+++ 00-starting-js-proj/scripts/util.js
@@ -0,0 +1,5 @@
+// export let apiKey = "afsdddsdfgh";
+export default "afsdddsdfgh";
+export let apiKey = "afsdddsdfgh";
+export let abc = "abc";
+export let xyz = "xyz";
