4a8d05fa35aa2431244494e5e19e7fad58aab35d
Anish Shukla <shuklaanish040@gmail.com>
2025-01-22 19:24:24 +0530

    udemy

--- 00-starting-js-proj/scripts/app.js
+++ 00-starting-js-proj/scripts/app.js
@@ -7,19 +7,33 @@
 // import { xyz } from "./util.js";
 // console.log(xyz);
 
-//let
-let userMeassage = "Anish Shukla";
-console.log(userMeassage);
-console.log(userMeassage);
+// //let
+// let userMeassage = "Anish Shukla";
+// console.log(userMeassage);
+// console.log(userMeassage);
 
-//const
-let clientMeassage = "Ravi Shukla";
-console.log(clientMeassage);
-console.log(clientMeassage);
+// //const
+// let clientMeassage = "Ravi Shukla";
+// console.log(clientMeassage);
+// console.log(clientMeassage);
 
-//operators
+// //operators
 
-// console.log(10===5);
-if (10 === 10) {
-  console.log("works");
+// // console.log(10===5);
+// if (10 === 10) {
+//   console.log("works");
+// }
+
+//Fuctions
+
+// function greet() {
+//   console.log("Anish");
+// }
+// greet();
+
+function greetUser(userName, meassage = "10") {
+  console.log(userName);
+  console.log(meassage);
 }
+greetUser("Shukla");
+greetUser("Ravi", "Shukla");
