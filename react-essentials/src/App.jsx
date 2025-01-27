dfab5b918ca92d8d757123fc803ea40c2122c2b2
Anish Shukla <shuklaanish040@gmail.com>
2025-01-27 19:20:35 +0530

    udemy

--- react-essentials/src/App.jsx
+++ react-essentials/src/App.jsx
@@ -4,13 +4,14 @@ function genRandomInt(max) {
 }
 
 function Header() {
+  const description = reactDescription[genRandomInt(2)];
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {reactDescription[genRandomInt(2)]} React concepts you will need for
-        almost any app you are going to build!
+        {description} React concepts you will need for almost any app you are
+        going to build!
       </p>
     </header>
   );
