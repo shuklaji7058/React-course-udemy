80cd0f6839e305a8f7116c1738cd99f5ea6c41f2
Anish Shukla <shuklaanish040@gmail.com>
2025-01-28 19:05:14 +0530

    udemy

--- 01-react-essentials/src/App.jsx
+++ 01-react-essentials/src/App.jsx
@@ -1,11 +1,15 @@
+import componentsImg from "./assets/components.png";
 import reactImg from "./assets/react-core-concepts.png";
-const reactDescription = ["Fundamental", "Crucial", "Core"];
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
 function Header() {
-  const description = reactDescription[genRandomInt(2)];
+  const description = reactDescriptions[genRandomInt(2)];
+
   return (
     <header>
       <img src={reactImg} alt="Stylized atom" />
@@ -18,12 +22,34 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The core UI building block."
+              image={componentsImg}
+            />
+            <CoreConcept title="Props" />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
       </main>
     </div>
   );
