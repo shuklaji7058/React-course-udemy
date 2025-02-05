da4b1e1c57d96bb60b4a0986a539863e0ab132d5
Anish Shukla <shuklaanish040@gmail.com>
2025-02-05 20:19:15 +0530

    udemy

--- 01-react-essentials/src/App.jsx
+++ 01-react-essentials/src/App.jsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 import CoreConcept from "./components/CoreConcept.jsx";
 import Header from "./components/Header/Header.jsx";
 import TabButton from "./components/TabButton.jsx";
-import { CORE_CONCEPTS } from "./data";
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
 
-function App() {
-  const [selectedTopic, setSelectedTopic] = useState("Please click a button");
+export default function App() {
+  const [selectedTopic, setSelectedTopic] = useState("components");
   function handleClick(selectedButton) {
     //for event handling
     setSelectedTopic(selectedButton);
@@ -33,7 +33,7 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleClick("components")}>
+            <TabButton onSelect={() => handleClick("state")}>
               COMPONENTS
             </TabButton>
             {/* <TabButton label="Components"></TabButton> */}
@@ -45,14 +45,18 @@ function App() {
             >
               JSX
             </TabButton>
-            <TabButton onSelect={() => handleClick("props")}>PROPS</TabButton>
-            <TabButton onSelect={() => handleClick("state")}>STATE</TabButton>
+            <TabButton onSelect={() => handleClick("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleClick("state")}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
   );
 }
-
-export default App;
