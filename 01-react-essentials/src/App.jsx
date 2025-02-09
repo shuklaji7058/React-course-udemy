408a58eb724d447281b882d25d24a3d5207dfb19
Anish Shukla <shuklaanish040@gmail.com>
2025-02-09 19:40:31 +0530

    udemy

--- 01-react-essentials/src/App.jsx
+++ 01-react-essentials/src/App.jsx
@@ -32,7 +32,7 @@ export default function App() {
       <Header />
       <main>
         <section id="core-concepts">
-          <h2>Core Concept</h2>
+          <h2>Core Concepts</h2>
           <ul>
             {CORE_CONCEPTS.map((conceptItem) => (
               <CoreConcept key={conceptItem.title} {...conceptItem} />
